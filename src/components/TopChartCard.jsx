9f6daa8310c37c9c8fb4385dd6f6844c70a6ba80
quitam <tamphamasd@gmail.com>
2023-08-25 03:11:14 +0700

    sync animation

--- src/components/TopChartCard.jsx
+++ src/components/TopChartCard.jsx
@@ -9,7 +9,7 @@ const TopChartCard = ({
   handlePause,
   handlePlay,
 }) => (
-  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2 animate-slideright">
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
       <img
